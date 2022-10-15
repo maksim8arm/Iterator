ac1306e1bd601b18719b182c6b0f334ddf388b6c
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-10-15 15:16:11 +0300

    add files

---
+++ src/com/company/Randoms.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.ints(min, max + 1).iterator().nextInt();
+            }
+        };
+    }
+}
